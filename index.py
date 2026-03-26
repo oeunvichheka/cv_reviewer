1605be1c2e525a554caede82d6659ea856624a3e
oeunvichheka <156062229+oeunvichheka@users.noreply.github.com>
2026-03-26 12:22:35 +0700

    Delete index.py

--- index.py
+++
@@ -1,8 +0,0 @@
-# api/index.py
-
-def handler(request):
-    return {
-        "statusCode": 200,
-        "headers": {"Content-Type": "text/plain"},
-        "body": "Hello! Your Vercel Python function is working 🚀"
-    }
